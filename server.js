153c74d9cb276d0ab93ed7a821083571f78cc729
tzlilhaim <tzlilhaim480@gmail.com>
2020-09-01 15:11:29 +0300

    server setup and health route

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const app = express()
+const path = require("path")
+const port = 8080
+const api = require("./server/routes/api")
+const bodyParser = require("body-parser")
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, "dist")))
+app.use("/scripts", express.static(__dirname + "/node_modules"))
+app.use("/", api)
+
+app.listen(port, function () {
+  console.log(`running server on port ${port}`)
+})
